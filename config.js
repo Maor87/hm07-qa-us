9a9444f58bc4825c9e5fc88318f2ae8ed164e465
maor <maorshaer87@gmail.com>
2024-07-02 16:31:01 +0300

    second renew server

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-a3607601-884e-46f7-9d23-df1d1ae0fadf.containerhub.tripleten-services.com"
+    API_URL: "https://cnt-d444ef0b-7c88-426c-bedd-ef4829f9dd19.containerhub.tripleten-services.com"
 }
