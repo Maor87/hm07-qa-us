148898b2bd732970835b26cf63747ca4642dbb13
maor <maorshaer87@gmail.com>
2024-07-03 14:21:01 +0300

    changing server once again

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-e125c0b2-a9a5-4b9e-a69e-59b31b5478a9.containerhub.tripleten-services.com"
+    API_URL: "https://cnt-498d0ed7-e35d-4b9e-849c-56c859a44ff8.containerhub.tripleten-services.com"
 }
